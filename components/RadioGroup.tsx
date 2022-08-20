dad9a738888fae93b4c30a6b9dc2a0d2766a8376
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-08-20 13:37:58 -0400

    Split RadioGroup into RadioGroup and react-hook-form wrapper

--- components/RadioGroup.tsx
+++ components/RadioGroup.tsx
@@ -3,20 +3,70 @@ import {
   FieldPath,
   FieldValues,
   RegisterOptions,
+  UseControllerProps,
+  useController,
   UseFormRegister,
 } from "react-hook-form"
 
-interface Props<T> {
+interface RHFRadioGroupProps<T>
+  extends UseControllerProps<T>,
+    Omit<RadioGroupProps, "onChange" | "onBlur" | "ref"> {
   name: FieldPath<T>
-  id: string
-  options: RegisterOptions<T>
-  register: UseFormRegister<T>
+}
+
+export const RHFRadioGroup = <T extends FieldValues>(
+  props: RHFRadioGroupProps<T>,
+) => {
+  const { name, rules, control, radioOptions, readOnly, className, disabled } =
+    props
+
+  const { field } = useController({
+    control,
+    name,
+    rules,
+  })
+
+  return (
+    <RadioGroup
+      onChange={(e) => {
+        field.onChange(e.target.value)
+      }}
+      inputRef={field.ref}
+      onBlur={field.onBlur}
+      name={field.name}
+      radioOptions={radioOptions}
+      selectedValue={field.value}
+      readOnly={readOnly}
+      className={className}
+      disabled={disabled}
+    />
+  )
+}
+
+interface RadioGroupProps {
+  name?: string
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
+  onBlur?: React.FocusEventHandler<HTMLInputElement>
+  inputRef?: React.LegacyRef<HTMLInputElement>
   radioOptions: [string | number, string][]
   readOnly?: boolean
+  disabled?: boolean
   className?: string
+  selectedValue?: string
 }
-export const RadioGroup = <T extends FieldValues>(props: Props<T>) => {
-  const { name, options, register, radioOptions, readOnly, className } = props
+
+export const RadioGroup = (props: RadioGroupProps) => {
+  const {
+    onChange,
+    onBlur,
+    inputRef: ref,
+    name,
+    radioOptions,
+    readOnly,
+    disabled,
+    className,
+    selectedValue,
+  } = props
 
   return (
     <div className={`radio-group row ${className ?? ""}`}>
@@ -24,16 +74,21 @@ export const RadioGroup = <T extends FieldValues>(props: Props<T>) => {
         return (
           <label
             key={i}
-            className={`${options.disabled ? "disabled" : ""} ${
+            className={`${disabled ? "disabled" : ""} ${
               readOnly ? "readOnly" : ""
             }`}
           >
             <input
-              {...register(name, options)}
               type="radio"
+              name={name}
+              onChange={onChange}
+              ref={ref}
+              onBlur={onBlur}
               value={value}
               readOnly={readOnly}
+              checked={selectedValue === value}
               onClick={(e) => readOnly && e.preventDefault()}
+              disabled={disabled}
             />
             <span>{label}</span>
           </label>
