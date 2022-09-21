111934a4b4f2ec45b9626625d9a5b1e4ff4c10e5
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-09-20 20:29:41 -0400

    Added message and links when risk assessment saved

--- pages/risk-assessments/new.tsx
+++ pages/risk-assessments/new.tsx
@@ -7,6 +7,8 @@ import RiskAssessmentEntry, {
 import { SubmitHandler, useForm } from "react-hook-form"
 import axios from "axios"
 import { RiskAssessmentFields } from "types/fields"
+import { RiskAssessmentAll } from "types/models"
+import Link from "next/link"
 
 const defaultValues: NullPartialRiskAssessmentFields = {
   compoundName: null,
@@ -77,6 +79,7 @@ const defaultValues: NullPartialRiskAssessmentFields = {
 
 const NewRiskAssessment: NextPage = () => {
   const [saveSuccessful, setSaveSuccessful] = useState<boolean | undefined>()
+  const [savedId, setSavedId] = useState<number | undefined>()
   const formMethods = useForm<NullPartialRiskAssessmentFields>()
 
   const { handleSubmit, reset } = formMethods
@@ -85,9 +88,10 @@ const NewRiskAssessment: NextPage = () => {
     data,
   ) => {
     await axios
-      .post("/api/risk-assessments", data)
+      .post<RiskAssessmentAll>("/api/risk-assessments", data)
       .then((res) => {
         setSaveSuccessful(true)
+        setSavedId(res.data.id)
       })
       .catch((reason) => {
         //TODO: Handle error
@@ -99,129 +103,156 @@ const NewRiskAssessment: NextPage = () => {
   return (
     <Layout>
       <div className="page">
-        <h1 style={{ marginTop: "0px" }}>New Risk Assessment</h1>
-        <form
-          onSubmit={handleSubmit(onSubmit, (errors) => {
-            console.log(errors)
-            setSaveSuccessful(false)
-          })}
-          autoComplete="off"
-        >
-          <RiskAssessmentEntry
-            values={defaultValues}
-            formMethods={formMethods}
-          />
-          <div>
-            <div className="row">
-              <button type="submit">Submit</button>
-              <button
-                type="button"
-                onClick={() => {
-                  const testValues: RiskAssessmentFields = {
-                    compoundName: "TEST COMPOUND",
-                    ingredients: [
-                      {
-                        id: null,
-                        chemicalId: 1,
-                        productId: 1,
-                        sdsId: 1,
-                        physicalForm: "cream",
-                        commercialProduct: {
-                          isCommercialProduct: true,
-                          name: "TEST COMMERCIAL PRODUCT",
-                          din: 123456,
-                          hasNoDin: false,
-                          hasProductMonographConcerns: true,
-                          concernsDescription:
-                            "TEST PRODUCT MONOGRAPH CONCERNS",
+        {!saveSuccessful || !savedId ? (
+          <>
+            <h1 style={{ marginTop: "0px" }}>New Risk Assessment</h1>
+            <form
+              onSubmit={handleSubmit(onSubmit, (errors) => {
+                console.log(errors)
+                setSaveSuccessful(false)
+              })}
+              autoComplete="off"
+            >
+              <RiskAssessmentEntry
+                values={defaultValues}
+                formMethods={formMethods}
+              />
+              <div>
+                <div className="row">
+                  <button type="submit">Submit</button>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      const testValues: RiskAssessmentFields = {
+                        compoundName: "TEST COMPOUND",
+                        ingredients: [
+                          {
+                            id: null,
+                            chemicalId: 1,
+                            productId: 1,
+                            sdsId: 1,
+                            physicalForm: "cream",
+                            commercialProduct: {
+                              isCommercialProduct: true,
+                              name: "TEST COMMERCIAL PRODUCT",
+                              din: 123456,
+                              hasNoDin: false,
+                              hasProductMonographConcerns: true,
+                              concernsDescription:
+                                "TEST PRODUCT MONOGRAPH CONCERNS",
+                            },
+                          },
+                          {
+                            id: null,
+                            chemicalId: 2,
+                            productId: 2,
+                            sdsId: 2,
+                            physicalForm: "cream",
+                            commercialProduct: {
+                              isCommercialProduct: false,
+                              name: null,
+                              din: null,
+                              hasNoDin: null,
+                              hasProductMonographConcerns: null,
+                              concernsDescription: null,
+                            },
+                          },
+                        ],
+                        complexity: "complex",
+                        averagePreparationAmount: {
+                          quantity: 5,
+                          unit: "g",
                         },
-                      },
-                      {
-                        id: null,
-                        chemicalId: 2,
-                        productId: 2,
-                        sdsId: 2,
-                        physicalForm: "cream",
-                        commercialProduct: {
-                          isCommercialProduct: false,
-                          name: null,
-                          din: null,
-                          hasNoDin: null,
-                          hasProductMonographConcerns: null,
-                          concernsDescription: null,
+                        crossContaminationRisk: false,
+                        hasVerificationSteps: false,
+                        haveAppropriateFacilities: true,
+                        isConcentrationHealthRisk: false,
+                        isPreparedOccasionally: true,
+                        isSmallQuantity: true,
+                        isWorkflowUninterrupted: false,
+                        workflowStandardsProcess:
+                          "TEST WORKFLOW INTERRUPTION MITIGATION STANDARDS DESCRIPTION",
+                        microbialContaminationRisk: false,
+                        requireEyeWashStation: false,
+                        requireSafetyShower: false,
+                        requireSpecialEducation: false,
+                        requireVentilation: true,
+                        riskLevel: "A",
+                        exposureRisks: {
+                          sds: {
+                            eye: true,
+                            oral: false,
+                            inhalation: true,
+                            skin: true,
+                            other: false,
+                            otherDescription: null,
+                          },
+                          productMonograph: {
+                            eye: true,
+                            oral: false,
+                            inhalation: false,
+                            skin: true,
+                            other: false,
+                            otherDescription: null,
+                          },
                         },
-                      },
-                    ],
-                    complexity: "complex",
-                    averagePreparationAmount: {
-                      quantity: 5,
-                      unit: "g",
-                    },
-                    crossContaminationRisk: false,
-                    hasVerificationSteps: false,
-                    haveAppropriateFacilities: true,
-                    isConcentrationHealthRisk: false,
-                    isPreparedOccasionally: true,
-                    isSmallQuantity: true,
-                    isWorkflowUninterrupted: false,
-                    workflowStandardsProcess:
-                      "TEST WORKFLOW INTERRUPTION MITIGATION STANDARDS DESCRIPTION",
-                    microbialContaminationRisk: false,
-                    requireEyeWashStation: false,
-                    requireSafetyShower: false,
-                    requireSpecialEducation: false,
-                    requireVentilation: true,
-                    riskLevel: "A",
-                    exposureRisks: {
-                      sds: {
-                        eye: true,
-                        oral: false,
-                        inhalation: true,
-                        skin: true,
-                        other: false,
-                        otherDescription: null,
-                      },
-                      productMonograph: {
-                        eye: true,
-                        oral: false,
-                        inhalation: false,
-                        skin: true,
-                        other: false,
-                        otherDescription: null,
-                      },
-                    },
-                    ppe: {
-                      mask: { required: true, type: "Disposable" },
-                      coat: { required: true, type: "designated" },
-                      gloves: { required: false, type: null },
-                      eyeProtection: { required: true },
-                    },
-                    preparationFrequency: "monthly",
-                    rationaleList: {
-                      automatic: [],
-                      additional: [
-                        "TEST ADDITIONAL RATIONALE 1",
-                        "TEST ADDITIONAL RATIONALE 2",
-                      ],
-                    },
-                    dateAssessed: new Date().toLocaleDateString("en-CA"),
-                  }
-                  reset(testValues, { keepDefaultValues: true })
-                }}
-              >
-                FILL WITH TEST
-              </button>
-              <button type="button" onClick={() => reset()}>
-                Reset
-              </button>
+                        ppe: {
+                          mask: { required: true, type: "Disposable" },
+                          coat: { required: true, type: "designated" },
+                          gloves: { required: false, type: null },
+                          eyeProtection: { required: true },
+                        },
+                        preparationFrequency: "monthly",
+                        rationaleList: {
+                          automatic: [],
+                          additional: [
+                            "TEST ADDITIONAL RATIONALE 1",
+                            "TEST ADDITIONAL RATIONALE 2",
+                          ],
+                        },
+                        dateAssessed: new Date().toLocaleDateString("en-CA"),
+                      }
+                      reset(testValues, { keepDefaultValues: true })
+                    }}
+                  >
+                    FILL WITH TEST
+                  </button>
+                  <button type="button" onClick={() => reset()}>
+                    Reset
+                  </button>
+                </div>
+                {saveSuccessful !== undefined && (
+                  <p style={{ color: saveSuccessful ? "green" : "red" }}>
+                    {saveSuccessful ? "Saved" : "Error"}
+                  </p>
+                )}
+              </div>
+            </form>
+          </>
+        ) : (
+          <div className="savedPrompt">
+            <p style={{ fontSize: "2.4rem", fontWeight: 600 }}>
+              The risk assessment for {formMethods.getValues("compoundName")}{" "}
+              has been saved.
+            </p>
+            <div style={{ display: "flex", columnGap: "3rem" }}>
+              <Link href="/risk-assessments/new" passHref>
+                <a
+                  onClick={() => {
+                    setSaveSuccessful(undefined)
+                    setSavedId(undefined)
+                    reset()
+                  }}
+                >
+                  Create another risk assessment
+                </a>
+              </Link>
+              <Link href={`/risk-assessments/${savedId}`} passHref>
+                <a>View created risk assessment</a>
+              </Link>
             </div>
-            {saveSuccessful !== undefined && (
-              <p style={{ color: saveSuccessful ? "green" : "red" }}>
-                {saveSuccessful ? "Saved" : "Error"}
-              </p>
-            )}
           </div>
-        </form>
+        )}
       </div>
       <style jsx>{`
         form {
