a52360664b0d68b0f7f93904b1c616ae96676c77
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-09-22 15:28:51 -0400

    Update pages/api/sds/[id].ts

--- pages/api/sds/[id].ts
+++ pages/api/sds/[id].ts
@@ -1,43 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { prisma } from "lib/prisma"
+import { SdsWithRelations } from "types/models"
+import { ApiBody } from "types/common"
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse<ApiBody<SdsWithRelations>>,
 ) {
   const {
     query: { id },
     method,
   } = req
 
-  //TODO: Implement other User methods
+  if (id === undefined) {
+    res.status(404).json({
+      error: { code: 404, message: "Missing required query parameter 'id'." },
+    })
+    return
+  }
+
   switch (method) {
     case "GET": {
       let sds
 
       try {
-        sds = await prisma.sDS.findUnique({
-          where: { id: parseInt(id as string) },
-          include: {
-            product: {
-              include: {
-                vendor: true,
-                chemical: true,
-              },
-            },
-            healthHazards: {
-              include: {
-                hazardCategory: {
-                  include: {
-                    hazardClass: true,
-                  },
-                },
-              },
-            },
-          },
-        })
+        sds = await getSdsById(parseInt(typeof id === "string" ? id : id[0]))
       } catch (error) {
-        //TODO: HANDLE ERROR
         console.log(error)
         res.status(500).json({
           error: { code: 500, message: "Encountered error with database." },
@@ -45,18 +33,50 @@ export default async function handler(
         return
       }
 
+      if (sds === null) {
+        res.status(500).json({
+          error: { code: 404, message: `SDS ${id} not found.` },
+        })
+        return
+      }
+
       res.status(200).json({ ...sds })
       return
     }
-    case "PATCH": {
-      //TODO: Implement PATCH method
+
+    case "PUT": {
+      //TODO: Implement PUT method
+      throw new Error("PUT method not implemented.")
     }
 
     default:
       res
-        .setHeader("Allow", ["GET", "PATCH"])
+        .setHeader("Allow", ["GET", "PUT"])
         .status(405)
         .json({ error: { code: 405, message: `Method ${method} Not Allowed` } })
       break
   }
+}
+
+export const getSdsById = async (id: number) => {
+  return await prisma.sDS.findUnique({
+    where: { id },
+    include: {
+      product: {
+        include: {
+          vendor: true,
+          chemical: true,
+        },
+      },
+      healthHazards: {
+        include: {
+          hazardCategory: {
+            include: {
+              hazardClass: true,
+            },
+          },
+        },
+      },
+    },
+  })
 }
