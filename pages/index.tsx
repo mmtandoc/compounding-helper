29bc86a8c5c51b9d3bbb43a2c4faf1790a77e7b3
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-09-30 10:18:05 -0400

    Updated Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,92 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <div className="page">
-        <h1 style={{ marginTop: "0px" }}>Compounding Helper - Home Page</h1>
-        <h2 style={{ marginBottom: "0" }}>Risk Assessments:</h2>
-        <ul style={{ marginTop: "0" }}>
-          <li>
-            <Link href="/risk-assessments">
-              <a>View Risk Assessments</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/risk-assessments/new">
-              <a>New Risk Assessment</a>
-            </Link>
-          </li>
-        </ul>
+        <h1>Compounding Helper - Home Page</h1>
+        <div className="data-links-row">
+          <div className="data-links">
+            <h2>Risk Assessments</h2>
+            <ul>
+              <li>
+                <Link href="/risk-assessments">
+                  <a>View Risk Assessments</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/risk-assessments/new">
+                  <a>New Risk Assessment</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="data-links">
+            <h2>Safety Data Sheets</h2>
+            <ul>
+              <li>
+                <Link href="/sds">
+                  <a>View Safety Data Sheets</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/sds/new">
+                  <a>New Safety Data Sheet</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="data-links">
+            <h2>Chemicals</h2>
+            <ul>
+              <li>
+                <Link href="/chemicals">
+                  <a>View Chemicals</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/chemicals/new">
+                  <a>New Chemical</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="data-links">
+            <h2>Products</h2>
+            <ul>
+              <li>
+                <Link href="/products">
+                  <a>View Products</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/products/new">
+                  <a>New Product</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
+      <style jsx>{`
+        .page > h1:first-of-type {
+          margin-top: 0;
+        }
+
+        .data-links-row {
+          display: flex;
+          flex-wrap: wrap;
+        }
+
+        .data-links {
+          flex: 1;
+        }
+
+        .data-links > h2 {
+          margin-bottom: 0;
+        }
+
+        .data-links > ul {
+          margin-top: 0;
+        }
+      `}</style>
     </Layout>
   )
 }
