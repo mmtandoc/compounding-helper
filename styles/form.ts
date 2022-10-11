4c40a4d4fc742fd2129e75e6cc3a8ef296cb8758
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-10-11 13:25:27 -0400

    Updated style/form.ts

--- styles/form.ts
+++ styles/form.ts
@@ -29,34 +29,48 @@ const form = css.global`
     width: 100%;
   }
 
-  .form-group > :where(label, .label) {
+  .form-group > :where(label, .label),
+  label.form-group,
+  .label.form-group {
     font-weight: 600;
     display: block;
     width: fit-content;
+  }
+
+  :where(.form-group
+      > :where(label, .label), :where(label, .label).form-group):not(:only-child) {
     margin-bottom: 0.3rem;
+  }
+
+  :where(label, .label).form-group {
+    font-weight: 600;
+  }
+
+  :where(.form-group > :where(label, .label), :where(label, .label).form-group)
+    > :where(span:only-child, :not(span:first-child)) {
+    font-weight: initial;
+  }
+
+  :where(.form-group > :where(label, .label), :where(label, .label).form-group)
+    > :where(span:only-child, :not(span:first-child))
+    * {
+    font-weight: initial;
   }
 
   .form-group.row > :where(label, .label) {
     margin-bottom: 0;
   }
 
-  :where(.form-group label, label.form-group) > span:first-child {
-    font-weight: 600;
-    display: block;
+  :where(.form-group:not(.row)
+      > :where(label, .label), :where(label, .label).form-group:not(.row))
+    > :where(span, p):first-child {
     width: fit-content;
     margin-bottom: 0.3rem;
-  }
-
-  :where(.form-group label, label.form-group) > :not(span:first-child) {
-    font-weight: initial;
+    display: block;
   }
 
   .label {
     font-weight: 600;
-  }
-
-  label > input:where([type="checkbox"], [type="radio"]) + span {
-    font-weight: normal;
   }
 
   label.disabled {
