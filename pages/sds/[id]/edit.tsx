fc2d91fae3cc38bb5dbe6be77c1b7d35176ae716
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-10-30 13:02:58 -0400

    Fixed SDS edit page

--- pages/sds/[id]/edit.tsx
+++ pages/sds/[id]/edit.tsx
@@ -1,14 +1,11 @@
-import axios from "axios"
+import EditForm from "components/common/data-pages/EditForm"
 import Layout from "components/Layout"
 import SdsEntry from "components/sds/SdsEntry"
+import { NullPartialSdsFields, sdsSchema } from "lib/fields"
 import SdsMapper from "lib/mappers/SdsMapper"
-import { NullPartialSdsFields } from "lib/fields"
 import { GetServerSideProps, NextPage } from "next"
-import Link from "next/link"
 import { useRouter } from "next/router"
 import { getSdsById } from "pages/api/sds/[id]"
-import { useState } from "react"
-import { SubmitHandler, useForm } from "react-hook-form"
 
 type EditSdsPageProps = {
   values: NullPartialSdsFields
@@ -18,60 +15,22 @@ const EditSdsPage: NextPage<EditSdsPageProps> = (props: EditSdsPageProps) => {
   const { values } = props
 
   const router = useRouter()
-
-  const [saveSuccessful, setSaveSuccessful] = useState<boolean | undefined>()
-
-  const formMethods = useForm<NullPartialSdsFields>({ defaultValues: values })
-  const { handleSubmit } = formMethods
-
-  const sdsId = parseInt(router.query.id as string)
-
-  const onSubmit: SubmitHandler<NullPartialSdsFields> = async (data) => {
-    await axios
-      .put(`/api/sds/${sdsId}`, data)
-      .then(() => {
-        setSaveSuccessful(true)
-        router.push(`/sds/${sdsId}`)
-      })
-      .catch((reason) => {
-        //TODO: Handle error
-        console.log(JSON.stringify(reason))
-        setSaveSuccessful(false)
-      })
-  }
+  const id = parseInt(router.query.id as string)
 
   return (
     <Layout>
       <div className="page">
         <h1 style={{ marginTop: "0px" }}>Edit SDS</h1>
-        <form
-          onSubmit={handleSubmit(onSubmit, (errors) => {
-            console.log(errors)
-            setSaveSuccessful(false)
-          })}
-          autoComplete="off"
-        >
-          <SdsEntry formMethods={formMethods} />
-          <div>
-            <div className="row">
-              <button type="submit">Save</button>
-              <Link href={`/sds/${sdsId}`} passHref>
-                <button type="button">Cancel</button>
-              </Link>
-            </div>
-            {saveSuccessful !== undefined && (
-              <p style={{ color: saveSuccessful ? "green" : "red" }}>
-                {saveSuccessful ? "Saved" : "Error"}
-              </p>
-            )}
-          </div>
-        </form>
+        <EditForm
+          id={id}
+          schema={sdsSchema}
+          values={values as NullPartialSdsFields}
+          apiEndpointPath="/api/sds"
+          urlPath="/sds"
+          entryComponent={SdsEntry}
+        />
       </div>
       <style jsx>{`
-        form {
-          align-self: center;
-        }
-
         .page {
           margin-bottom: 5rem;
         }
