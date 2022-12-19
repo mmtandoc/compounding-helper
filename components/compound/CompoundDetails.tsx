7314cfe5d28ffdd071692f25ac034305a70e403d
Michael Tandoc <michael.tandoc@ryerson.ca>
2022-12-19 14:20:50 -0500

    Added MFR actions to CompoundDetails

--- components/compound/CompoundDetails.tsx
+++ components/compound/CompoundDetails.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link"
 import React from "react"
+import useSWR from "swr"
 
+import Button from "components/common/Button"
 import Fieldset from "components/common/forms/Fieldset"
 import { FormGroup } from "components/common/forms/FormGroup"
+import Spinner from "components/common/Spinner"
 import { IngredientDetails } from "components/compound/ingredient/IngredientDetails"
-import { CompoundWithIngredients } from "types/models"
+import { CompoundWithIngredients, MfrAll } from "types/models"
 
 type Props = {
   data: CompoundWithIngredients
@@ -34,6 +38,7 @@ const CompoundDetails = (props: Props) => {
             <span className="label">Notes:</span>
             <textarea value={data.notes ?? "None"} readOnly={true} cols={20} />
           </FormGroup>
+          <MfrsActions data={data} />
         </>
       )}
       <style jsx global>{`
@@ -57,6 +62,44 @@ const CompoundDetails = (props: Props) => {
         }
       `}</style>
     </div>
+  )
+}
+
+const MfrsActions = (props: { data: CompoundWithIngredients }) => {
+  const { data } = props
+
+  const { data: mfrs, error: mfrsError } = useSWR<MfrAll[]>(
+    `/api/compounds/${data.id}/mfrs`,
+  )
+
+  if (mfrsError) {
+    console.error(mfrsError)
+  }
+
+  const isLoading = !mfrs && !mfrsError
+  return (
+    <FormGroup row>
+      {!isLoading ? (
+        <>
+          {mfrs && mfrs.length > 0 ? (
+            <>
+              <Link href={`/compounds/${data.id}/mfrs/latest`}>
+                <Button size="small">View latest MFR</Button>
+              </Link>
+              <Link href={`/compounds/${data.id}/mfrs`}>
+                <Button size="small">View all MFRs</Button>
+              </Link>
+            </>
+          ) : (
+            <Link href={`/compounds/${data.id}/mfrs/new`}>
+              <Button size="small">Create MFR</Button>
+            </Link>
+          )}
+        </>
+      ) : (
+        <Spinner />
+      )}
+    </FormGroup>
   )
 }
 
