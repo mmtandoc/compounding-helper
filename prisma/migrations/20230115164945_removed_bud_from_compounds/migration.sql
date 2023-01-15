38a03bc62459cb13b657fff515aad5559f70828c
Michael Tandoc <michael.tandoc@ryerson.ca>
2023-01-15 11:50:23 -0500

    Removed beyondUseDate column from compounds table

---
+++ prisma/migrations/20230115164945_removed_bud_from_compounds/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `beyondUseDate` on the `compounds` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "compounds" DROP COLUMN "beyondUseDate";
