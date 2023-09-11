f647e386e14b3c74354988fb613915fcda49d4c2
Michael Tandoc <michael.tandoc@ryerson.ca>
2023-09-11 14:48:44 -0400

    Fix /api/users/current to return undefined if not logged in

--- pages/api/users/current.ts
+++ pages/api/users/current.ts
@@ -1,24 +1,31 @@
-import { sendJsonError, withSession } from "lib/api/utils"
+import { NextApiHandler } from "next"
+
+import { getSession, sendJsonError, withSession } from "lib/api/utils"
 import { ApiBody } from "types/common"
 import { UserWithPharmacy } from "types/models"
 
-const handler = withSession<ApiBody<UserWithPharmacy | undefined>>(
-  async (req, res) => {
-    const { method, session } = req
+const handler: NextApiHandler<ApiBody<UserWithPharmacy | undefined>> = async (
+  req,
+  res,
+) => {
+  const { method } = req
 
-    switch (method) {
-      case "GET": {
-        return res.status(200).json(session.appUser)
-      }
-      default:
-        sendJsonError(
-          res.setHeader("Allow", ["GET"]),
-          405,
-          `Method ${method} Not Allowed`,
-        )
-        break
+  switch (method) {
+    case "GET": {
+      const session = await getSession({ req, res })
+
+      if (!session) return res.status(204).send(undefined)
+
+      return res.status(200).json(session.appUser)
     }
-  },
-)
+    default:
+      sendJsonError(
+        res.setHeader("Allow", ["GET"]),
+        405,
+        `Method ${method} Not Allowed`,
+      )
+      break
+  }
+}
 
 export default handler
