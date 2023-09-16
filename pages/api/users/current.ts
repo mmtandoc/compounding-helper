05f97edbf8783da0a54ab497f84c214e4c7105e7
Michael Tandoc <michael.tandoc@ryerson.ca>
2023-09-16 13:12:01 -0400

    Add "requireAuth" option for withSession function

--- pages/api/users/current.ts
+++ pages/api/users/current.ts
@@ -1,31 +1,27 @@
-import { NextApiHandler } from "next"
-
-import { getSession, sendJsonError, withSession } from "lib/api/utils"
+import { sendJsonError, withSession } from "lib/api/utils"
 import { ApiBody } from "types/common"
 import { UserWithPharmacy } from "types/models"
 
-const handler: NextApiHandler<ApiBody<UserWithPharmacy | undefined>> = async (
-  req,
-  res,
-) => {
-  const { method } = req
+const handler = withSession<ApiBody<UserWithPharmacy | undefined>>(
+  async (req, res) => {
+    const { method, session } = req
 
-  switch (method) {
-    case "GET": {
-      const session = await getSession({ req, res })
+    switch (method) {
+      case "GET": {
+        if (!session) return res.status(204).send(undefined)
 
-      if (!session) return res.status(204).send(undefined)
-
-      return res.status(200).json(session.appUser)
+        return res.status(200).json(session.appUser)
+      }
+      default:
+        sendJsonError(
+          res.setHeader("Allow", ["GET"]),
+          405,
+          `Method ${method} Not Allowed`,
+        )
+        break
     }
-    default:
-      sendJsonError(
-        res.setHeader("Allow", ["GET"]),
-        405,
-        `Method ${method} Not Allowed`,
-      )
-      break
-  }
-}
+  },
+  { requireAuth: false },
+)
 
 export default handler
