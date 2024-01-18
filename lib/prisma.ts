2bc6d32c3d45cae86bda4ff0df29a96938272df8
Michael Tandoc <michael.tandoc@torontomu.ca>
2024-01-18 15:17:54 -0500

    Fix deleteMany CASL extension to include required relations when querying to check for permissions

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -266,6 +266,7 @@ export function withCaslAbilities(user: AppUser) {
                     args.where,
                   ],
                 },
+                include: requiredIncludes?.[model],
               },
             )
 
